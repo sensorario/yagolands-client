3956cc39b37d9bdf14f6fbab6145ad0416f01a70
Simone Gentili <sensorario@gmail.com>
2022-08-21 00:12:00 +0200

    feat: add comment

--- modules/generator/generator.js
+++ modules/generator/generator.js
@@ -1,3 +1,4 @@
+// this code is duplicated in client and server, ...
 let generate = function () {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
