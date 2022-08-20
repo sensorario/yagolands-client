062d65a67a781ebaf428e82dd3424a03eb9b7c27
Simone Gentili <sensorario@gmail.com>
2022-08-21 00:01:59 +0200

    feat: glue cookie with current session

---
+++ modules/generator/generator.js
@@ -0,0 +1,12 @@
+let generate = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + s4() + '-' + s4();
+}
+
+export default function generator() {
+    return {
+        generate,
+    }
+};
