f68261094cff17d34dcb441388233aa6ea12d48f
Massimo Artizzu <maxart.x@gmail.com>
2022-09-14 23:31:23 +0200

    feat(canvas): :sparkles: add zoom capability on mouse wheel

--- modules/ui/canvas.js
+++ modules/ui/canvas.js
@@ -5,7 +5,7 @@ const buildings = [];
 
 const BASE_CELL_SIZE = 50;
 const BORDER_WIDTH = 0.04;
-const FONT_SIZE = 16;
+const FONT_SIZE = 12;
 let zoom = 1;
 let currentCellSize = BASE_CELL_SIZE * zoom;
 
@@ -22,14 +22,41 @@ const resizeObserver = new ResizeObserver(([entry]) => {
 });
 resizeObserver.observe(canvas);
 
+/**
+ * Return the coordinates of the cell at the given x,y coordinates of the canvas
+ * @param {number} x x coord
+ * @param {number} y y coord
+ * @return {number[]}
+ */
+export const cellAt = (x, y) => {
+    const diffX = x - xShift;
+    const diffY = y - yShift;
+    return [Math.floor(diffX / currentCellSize), Math.floor(diffY / currentCellSize)];
+};
+
+/**
+ * Multiply the zoom level by the given factor, clamping it at [0.2, 5],
+ * centering the effect on the given canvas coordinates
+ * @param {number} x x coord
+ * @param {number} y y coord
+ * @param {number} factor factor to multiply the zoom level with
+ */
+export const zoomAt = (x, y, factor) => {
+    const diffX = xShift - x;
+    const diffY = yShift - y;
+    zoom = Math.max(Math.min(zoom * factor, 5), 0.2);
+    currentCellSize = BASE_CELL_SIZE * zoom;
+    xShift = x + diffX * factor;
+    yShift = y + diffY * factor;
+    render();
+};
+
 export const render = () => {
     context.fillStyle = '#eee';
     context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    const xStart = Math.floor(xShift / -currentCellSize);
-    const yStart = Math.floor(yShift / -currentCellSize);
-    const xEnd = Math.floor((canvasWidth - xShift) / currentCellSize);
-    const yEnd = Math.floor((canvasHeight - yShift) / currentCellSize);
+    const [xStart, yStart] = cellAt(0, 0);
+    const [xEnd, yEnd] = cellAt(canvasWidth, canvasHeight);
     const innerCellSize = currentCellSize * (1 - 2 * BORDER_WIDTH);
     for (let yIdx = yStart; yIdx <= yEnd; yIdx++) {
         for (let xIdx = xStart; xIdx <= xEnd; xIdx++) {
@@ -38,11 +65,11 @@ export const render = () => {
             context.fillStyle = xIdx || yIdx ? '#fff' : '#fdd';
             context.fillRect(x, y, innerCellSize, innerCellSize);
             context.fillStyle = '#333';
-            context.font = `normal ${FONT_SIZE * zoom}px`;
+            context.font = `${FONT_SIZE * zoom}px sans-serif`;
             context.textAlign = 'center';
             context.textBaseline = 'middle';
             context.fillText(
-                `${xIdx}, ${yIdx}`,
+                `${xIdx},${yIdx}`,
                 x + currentCellSize * BORDER_WIDTH + innerCellSize / 2,
                 y + currentCellSize * BORDER_WIDTH + innerCellSize / 2
             );
@@ -71,4 +98,8 @@ canvas.addEventListener('pointermove', ({ pageX, pageY }) => {
         render();
         panStart = { x: pageX, y: pageY };
     }
+});
+canvas.addEventListener('wheel', ({ pageX, pageY, deltaY }) => {
+    const { top, left } = canvas.getBoundingClientRect();
+    zoomAt(pageX - left, pageY - top, 1.1 ** (deltaY / -150));
 });
